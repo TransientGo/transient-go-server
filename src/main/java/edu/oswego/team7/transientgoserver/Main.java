dcb0e5b6bd9bb1ae2d86f765ec35ca14993d1242
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2016-12-03 21:20:38 -0500

    Change port

--- src/main/java/edu/oswego/team7/transientgoserver/Main.java
+++ src/main/java/edu/oswego/team7/transientgoserver/Main.java
@@ -36,7 +36,7 @@ import java.sql.Connection;
  */
 public class Main {
     public static void main(String[] args) {
-        port(5050);
+        port(Integer.valueOf(System.getenv("PORT")));
         Gson gson = new Gson();
         get("/", (request, response) -> "Welcome to Transient-Go Server");
         get("/create", (request, response) -> createUsersTable(), gson::toJson);
