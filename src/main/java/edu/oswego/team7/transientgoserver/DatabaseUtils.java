51f7dbb66538ea0d4d32948aae8501866f89640a
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2016-12-04 17:27:22 -0500

    Modify DatabaseUtils.java

--- src/main/java/edu/oswego/team7/transientgoserver/DatabaseUtils.java
+++ src/main/java/edu/oswego/team7/transientgoserver/DatabaseUtils.java
@@ -133,7 +133,7 @@ class DatabaseUtils {
             //appendProc.execute();
             //PreparedStatement pstmt = connection.prepareStatement("UPDATE users SET transient_ivorns = ? WHERE user_id = ?");
             //pstmt.setArray(1, appendProc.getArray(1));
-            PreparedStatement pstmt = connection.prepareStatement("UPDATE users SET transient_ivorns = array_append('{'s1','s2'}', 's3') WHERE user_id = ?");
+            PreparedStatement pstmt = connection.prepareStatement("UPDATE users SET transient_ivorns = array_append('{\"s1\",\"s2\"}', \"s3\") WHERE user_id = ?");
             //pstmt.setString(1, ivorn);
             pstmt.setString(1, id);
             if(pstmt.executeUpdate() > 0)
